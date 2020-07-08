7bdbff7963649faa1c9259be0ed2f509b3570560
Dindin Abduloh <50541680+adin17-alt@users.noreply.github.com>
2020-07-08 15:00:05 +0700

    setting routes di server js

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+
+
+// panggil routes
+var routes = require('./routes');
+routes(app);
 // parse aplication json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
