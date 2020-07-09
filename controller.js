eb8f682ce4e993570627e3c20e52d0997c17b5b3
Dindin Abduloh <50541680+adin17-alt@users.noreply.github.com>
2020-07-09 10:14:08 +0700

    menampilkan data dari tabel mhs

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function(req, res) {
     response.ok("Aplikasi berjalan", res)
 };
+
+// Emnamoilkan semua data MHS
+exports.tampilsemuamahasiswa = function(req, res) {
+    connection.query('SELECT * FROM mhs', function(error, rows, fileds) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
+};
