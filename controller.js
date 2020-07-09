95e090394da4b4a32709cf17f36e7fb24c13fc91
Dindin Abduloh <50541680+adin17-alt@users.noreply.github.com>
2020-07-09 10:29:38 +0700

    Menampilkan data berdasarkan id

--- controller.js
+++ controller.js
@@ -11,9 +11,22 @@ exports.index = function(req, res) {
 exports.tampilsemuamahasiswa = function(req, res) {
     connection.query('SELECT * FROM mhs', function(error, rows, fileds) {
         if (error) {
-            connection.log(error);
+            console.log(error);
         } else {
             response.ok(rows, res)
         }
     });
+};
+
+// Menampilkan data MHS berdasarkan id
+exports.tampilberdasarkanid = function(req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM mhs WHERE id = ?', [id],
+        function(error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok(rows, res);
+            }
+        });
 };
